8000ca914f172c000381dcde7718577b94fe5f21
DevEss <dev.shahryar@gmail.com>
2022-12-18 17:17:29 +0330

    A bug fixed

--- client/src/components/main.jsx
+++ client/src/components/main.jsx
@@ -10,7 +10,7 @@ export default function Main() {
                 <div className="grid justify-items-center">
                     <input
                         type="text"
-                        className="w-1/2 h-8 mt-10 rounded-md text-center outline-none border-2 transition-all duration-300 bg-light text-black border-gray dark:bg-gray dark:text-light dark:border-light focus:border-primary dark:focus:border-primary"
+                        className="w-full sm:w-1/2 h-8 mt-10 rounded-md text-center outline-none border-2 transition-all duration-300 bg-light text-black border-gray dark:bg-gray dark:text-light dark:border-light focus:border-primary dark:focus:border-primary"
                     />
                 </div>
             </form>
