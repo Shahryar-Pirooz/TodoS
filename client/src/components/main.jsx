6aa1cd9878213ce8aeeb2a260ec7065ecc139028
DevEss <dev.shahryar@gmail.com>
2022-12-20 13:19:33 +0330

    Axios installed

--- client/src/components/main.jsx
+++ client/src/components/main.jsx
@@ -1,19 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Main() {
+    const [value, setValue] = useState('')
+    const handleOnChange = (e) => {
+        setValue(e.target.value)
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(value)
+        setValue('')
+    }
     return (
         <>
             <h1 className="font-quick-sand font-thin text-6xl text-center pt-40 transition-all duration-300 text-gray hover:text-primary hover:font-bold dark:text-light  dark:hover:text-primary dark:hover:font-bold">
                 TodoS
             </h1>
-            <form action="">
-                <div className="grid justify-items-center">
+            <div className="grid justify-items-center">
+                <form
+                    method="get"
+                    className="w-screen grid justify-items-center"
+                    onSubmit={handleSubmit}
+                >
                     <input
                         type="text"
                         className="w-full sm:w-1/2 h-8 mt-10 rounded-md text-center outline-none border-2 transition-all duration-300 bg-light text-black border-gray dark:bg-gray dark:text-light dark:border-light focus:border-primary dark:focus:border-primary"
+                        name="title"
+                        placeholder="Enter your Title task"
+                        value={value}
+                        onChange={handleOnChange}
                     />
-                </div>
-            </form>
+                </form>
+            </div>
         </>
     )
 }
