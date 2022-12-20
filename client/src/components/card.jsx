c3e32dc6460fcdd0225f1d821a47410659f8a428
DevEss <dev.shahryar@gmail.com>
2022-12-20 16:10:34 +0330

    some card's class changed

--- client/src/components/card.jsx
+++ client/src/components/card.jsx
@@ -5,15 +5,15 @@ function Card() {
     const textStyle =
         'font-quick-sand transition-all duration-300 text-black dark:text-light'
     const iconStyle =
-        'w-full h-4 mx-2 rounded-sm text-black hover:bg-primary transition-all duration-300 hover:text-light hover:cursor-pointer dark:text-light dark:hover:text-gray'
+        'w-full h-full mx-2 py-2 rounded-sm text-black  hover:bg-primary transition-all duration-300 hover:text-light hover:cursor-pointer dark:text-light dark:hover:text-gray'
 
     return (
-        <div className="columns-3 gap-5 transition-all duration-300 bg-light dark:bg-gray">
+        <div className="h-8 columns-3 gap-5 text-center transition-all duration-300 bg-light dark:bg-gray">
             <div className="w-full">
                 <p className={textStyle}>1</p>
             </div>
             <div className="w-full">
-                <h4 className={textStyle + ' text-center'}>Test</h4>
+                <h4 className={textStyle}>Test</h4>
             </div>
             <div className="w-full columns-3">
                 <PencilIcon className={iconStyle} />
