3f81e6b10ba5293103f5b142bb6a1e0fa63907ad
DevEss <dev.shahryar@gmail.com>
2022-12-21 12:11:15 +0330

    card icons onclick

--- client/src/components/card.jsx
+++ client/src/components/card.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import { PencilIcon, TrashIcon, CheckIcon } from '@heroicons/react/24/solid'
 
 function Card(props) {
-    const { title, done, date } = props.data
-    console.log(props.data)
-    const textStyle =
-        'font-quick-sand transition-all duration-300 text-black dark:text-light'
-    const iconStyle =
-        'w-full h-full mx-2 py-2 rounded-sm text-black  hover:bg-primary transition-all duration-300 hover:text-light hover:cursor-pointer dark:text-light dark:hover:text-gray'
+    const { _id, title, done, date } = props.data
+    const textStyle = done
+        ? 'font-quick-sand transition-all duration-300 line-through text-gray dark:text-gray'
+        : 'font-quick-sand transition-all duration-300 text-black dark:text-light'
 
+    const iconStyle =
+        'w-full h-full mx-2 py-2 rounded-xl text-black  hover:bg-primary transition-all duration-300 hover:text-light hover:cursor-pointer dark:text-light dark:hover:text-gray'
+    const editTask = (id) => {}
+    const removeTask = (id) => {}
+    const doneTask = (id) => {}
     return (
         <div className="h-8 columns-3 gap-5 text-center transition-all duration-300 bg-light dark:bg-gray">
             <div className="w-full">
@@ -18,9 +21,18 @@ function Card(props) {
                 <h4 className={textStyle}>{title}</h4>
             </div>
             <div className="w-full columns-3">
-                <PencilIcon className={iconStyle} />
-                <TrashIcon className={iconStyle} />
-                <CheckIcon className={iconStyle} />
+                <PencilIcon
+                    className={iconStyle}
+                    onClick={() => editTask(_id)}
+                />
+                <TrashIcon
+                    className={iconStyle}
+                    onClick={() => removeTask(_id)}
+                />
+                <CheckIcon
+                    className={iconStyle}
+                    onClick={() => doneTask(_id)}
+                />
             </div>
         </div>
     )
